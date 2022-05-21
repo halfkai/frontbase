773130de7a5cada711e8fe3661987fa1582a1674
halfkai <0x158105@gmail.com>
2022-05-21 12:42:37 +0800

    style: add normalize.css

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
+import 'normalize.css';
+import 'uno.css';
+
 import App from './App.vue';
 import router from './router';
 import stores from './stores';
 import i18n from './i18n';
-import 'uno.css';
 
 const app = createApp(App);
 

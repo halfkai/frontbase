491359c64e5f9bc6d59643984ba6e5b935a11f8d
halfkai <0x158105@gmail.com>
2022-05-21 22:40:32 +0800

    feat: update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', component: () => import('@/views/Home/index.vue') },
+    { path: '/about', component: () => import('@/views/About/index.vue') },
   ],
 });
 

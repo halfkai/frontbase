c8ab404acceee8493cc8d7dbbc51472fda8cf4ae
halfkai <0x158105@gmail.com>
2022-05-22 00:05:35 +0800

    style: set global font

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 <style>
 #app {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    Ubuntu, Cantarell, 'Open Sans', 'Arial', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;

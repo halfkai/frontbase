e775539eebd4ec5965944046e24e4df23cf63375
halfkai <0x158105@gmail.com>
2022-05-21 19:17:15 +0800

    feat (components): init SvgIcon and BaseLink

---
+++ src/components/BaseLink.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import SvgIcon from './SvgIcon.vue';
+
+interface Props {
+  icon?: string
+  content?: string
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <a>
+    <SvgIcon
+      v-if="props.icon"
+      :id="props.icon"
+      w-1em
+      h-1em
+      inline-block
+      align-sub
+    />
+    <span>{{ content }}</span>
+  </a>
+</template>
---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface Props {
+  fileUrl?: string
+  id: string
+}
+const props = withDefaults(defineProps<Props>(), {
+  fileUrl: '/icons.svg',
+});
+
+const imgURL = computed(() => new URL(`${props.fileUrl}#${props.id}`, import.meta.url).href);
+</script>
+
+<template>
+  <svg>
+    <use :xlink:href="imgURL" />
+  </svg>
+</template>

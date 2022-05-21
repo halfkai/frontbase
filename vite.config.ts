89085e0c8f7eed20f4dedb572ac9737a24ca7ebd
halfkai <0x158105@gmail.com>
2022-05-21 12:41:36 +0800

    chore (deps): update dev env deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import legacy from '@vitejs/plugin-legacy';
 import { resolve } from 'path';
 import Unocss from 'unocss/vite';
 import { presetAttributify, presetUno } from 'unocss';
+// import { createStyleImportPlugin, AndDesignVueResolve } from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,11 +15,17 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    legacy({
+      targets: ['default', 'not IE 11'],
+    }),
     Unocss({
       presets: [
         presetUno(),
         presetAttributify(),
       ],
     }),
+    // createStyleImportPlugin({
+    // resolves: [AndDesignVueResolve()],
+    // }),
   ],
 });

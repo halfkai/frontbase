0f5190725e5bf3e1d61e272cc7924ca524daf9fa
halfkai <0x158105@gmail.com>
2022-05-25 22:40:01 +0800

    refactor (style): extract global style into separated file

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,3 @@
 <template>
   <router-view />
 </template>
-
-<style>
-#app {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Arial', 'Helvetica Neue', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
